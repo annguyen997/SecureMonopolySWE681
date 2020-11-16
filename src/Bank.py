0ee5fab44772aaad0bb250fef84f8eb8345e695e
An Nguyen <anguye60@masonlive.gmu.edu>
2020-11-16 01:24:19 -0500

    Additional updates

---
+++ src/Bank.py
@@ -0,0 +1,4 @@
+class Bank: 
+    TOTAL_MONETARY = 20580
+
+    
--- src/Player.py
+++ src/Player.py
@@ -1,18 +1,28 @@
 import TwoDice 
-from Board import TILES_LIST 
+import Board
 
 class Player():
 
-    PLAYER_TITLES = ["Regular", "Banker", "Accountant"]
+    PLAYER_TITLES = ["Regular", "Banker", "Auctioneer"]
 
     def __init__(self, id, name):
         self.id = id
         self.name = name
-        self.moneyAmount = 0 
+        self.money = 0  #Revision for later - each player gets $1500
         self.position = 0
         self.double = 0
 
+    #Get the monetary value of the player
+    def getMonetaryValue(self): 
+        return self.money 
+    
+    #Get the position of the player on the board
+    def getPosition(self): 
+        return self.position 
+    
+    #Move the player across the board when it is their turn 
     def move(self):
+        #Roll the dice to move the player 
         self.position += TwoDice.rollDice() 
 
         #Check if player rolled doubles 
@@ -22,14 +32,18 @@ class Player():
 
             #Check if the return total is zero, indicating player needs to go to jail
             if (add_total == 0): 
-                #TODO: Go to jail logic here
-                self.position = 11
+                self.position = Board.TILES_JAIL[0]
+                break
             else: 
                 self.position += add_total
 
-            #Check if player has cycled through the board
-            if (self.position > len(TILES_LIST)):
-                self.position = self.position - len(TILES_LIST)
+        #Check if player has cycled through the board
+        if (self.position > len(Board.TILES_LIST)):
+            self.position = self.position - len(Board.TILES_LIST)
+                
+            #Collect more money 
+        
+
     
 
         
