55aaf974484a5a278a701168bdf325c649cf2b6b
chuck <ttran49@masonlive.gmu.edu>
2020-11-30 18:48:26 -0400

    fixed the commit bs

--- src/Driver.py
+++ src/Driver.py
@@ -132,7 +132,7 @@ class Driver:
     def __StartSession(self, user):
 
         try:
-            with open("Fuq_M3_uP_DazDy.txt", "a") as file:
+            with open("./Fuq_M3_uP_DazDy.txt", "a") as file:
 
                 file.writelines(str(user)
                                 + ":"
@@ -157,33 +157,25 @@ class Driver:
         key_from_storage = storage[32:]
         '''
         try:
-            with open('plEAzeDAddyNOO.txt', 'r') as file:
+            with open('./plEAzeDAddyNOO.txt', 'r') as file:
                 data = file.readlines()
             
                 for user_data in data:
                     # find user in list
                     if (str(user) == str(user_data.split(':')[0])):
-<<<<<<< HEAD
                         stored_hash = base64.b64decode(user_data.split(':')[-1])
-=======
-                        stored_hash = user_data.split(':')[-1]
->>>>>>> a0ab70da55769c9c318335cda4c64012bae9b446
 
                         # calculate hash and compare
                         # i need to convert to normal int from str, 
                         salt_from_storage = stored_hash[:32] # 32 is the length of the salt
 
                         # need to check compability
-<<<<<<< HEAD
                         if (self.__calcHash(password, salt_from_storage) == stored_hash[32:]):
                             print("[!] LOG: User '%s' successful logged in"
                                 % (str(user)))
 
                             # NEED SESSION ID
-=======
-                        if (calcHash(password, salt_from_storage) == key_from_storage = storage[32:-1]):
-                            print("[!] LOG: User '%s' successful logged in" % (str(user))
->>>>>>> a0ab70da55769c9c318335cda4c64012bae9b446
+                            self.__StartSession(str(user))
                             return True
                         else:
                             print("[!] LOG: User - '%s' authentication failed - user auth FAILED"
