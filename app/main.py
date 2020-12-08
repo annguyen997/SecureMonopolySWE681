60bb6d09100b821941d4abf9d7c4f33bda2516ba
HoaSec <37164935+HoaSec@users.noreply.github.com>
2020-12-08 17:02:42 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template, flash, redirect,render_template,request
+from flask import Flask, render_template, flash,make_response, redirect,render_template,request
 from forms import LoginForm,RegisterForm
 from driver import Driver 
+import socket
+import sys
+
+def sendToController(data):
+	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	clientSocket.connect(("127.0.0.1",2004))
+	clientSocket.send(data.encode())
+	dataFromServer = clientSocket.recv(1024)
+	return(dataFromServer.decode())
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'derp_derp'
 
@@ -12,12 +23,27 @@ def index():
     return render_template('base.html', title='Welcome')
 
 
+@app.route('/new_game', methods=['GET', 'POST'])
+def new_game():
+	if(request.method == 'GET' and 'sessionID' in request.cookies and 'userName' in request.cookies):
+		print(request.cookies)
+		output=sendToController('{"mana_":["createGame"],"username_": "'+request.cookies.get('userName')+'","sessionID_":"'+request.cookies.get('sessionID')+'"}')
+		return render_template('text_noform.html', title="You're good!", message=output)
+		#{"mana_":["createGame"],"username_": "Hoa_test1","sessionID_":"TFdcXBlITLFyCAUsI7hSOxuAHZdfFYj1oVDmYvqItwY="}
+	else:
+		return render_template('text_noform.html', title="You're good!", message="Please login or create an account")
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 	if request.method == 'POST':
-		 registration = Driver()
-		 output=registration.createUser(request.form['username'],request.form['password'])
-		 return render_template('text_noform.html', title="You're good!", message=output)
+		 output=sendToController('{"user_": ["Create","'+request.form['username']+'","'+request.form['password']+'"]}')
+		 #print(output)
+		 #output=registration.createUser(request.form['username'],request.form['password'])
+		 resp=make_response(render_template('text_noform.html', title="You're good!", message="Registration successful, logged in with token: "+output))
+		 resp.set_cookie('sessionID',output)
+		 resp.set_cookie('userName',request.form['username'])
+		 return resp
 	if request.method == 'GET':
 	    form = RegisterForm()
 	    if form.validate_on_submit():
@@ -29,12 +55,21 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}'.format(
-            form.username.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
+	if request.method == 'POST':
+		 output=sendToController('{"user_": ["Authenticate","'+request.form['username']+'","'+request.form['password']+'"]}')
+		 #print(output)
+		 #output=registration.createUser(request.form['username'],request.form['password'])
+		 resp=make_response(render_template('text_noform.html', title="You're good!", message="Authentication successful, logged in with token: "+output))
+		 resp.set_cookie('sessionID',output)
+		 resp.set_cookie('userName',request.form['username'])
+		 return resp
+	if request.method == 'GET':
+	    form = LoginForm()
+	    if form.validate_on_submit():
+	        flash('Login requested for user {}'.format(
+	            form.username.data))
+	        return redirect('/index')
+	    return render_template('login.html', title='Sign In', form=form)
 
 
 if __name__ == "__main__":
