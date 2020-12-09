0bee1bb266d029ab6a3f3f6e72c68f44ecd5cfc7
HoaSec <37164935+HoaSec@users.noreply.github.com>
2020-12-09 14:08:28 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,7 +13,10 @@ def sendToController(data):
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'derp_derp'
+f = open("secret_key.txt", "r")
+print(f.readline())
+f.close() 
+app.config['SECRET_KEY'] = str(f)
 
 @app.route("/")
 def default():
@@ -28,7 +31,35 @@ def new_game():
 	if(request.method == 'GET' and 'sessionID' in request.cookies and 'userName' in request.cookies):
 		output=sendToController('{"mana_":["createGame"],"username_": "'+request.cookies.get('userName')+'","sessionID_":"'+request.cookies.get('sessionID')+'"}')
 		#resp.set_cookie('sessionID',output)
-		return render_template('text_noform.html', title="You're good!", message="Here is your game ID, please use this to join a new game"+output)
+		if (output is None or output==""):
+			return render_template('text_noform.html', title="You're good!", message="There was some error.. ")
+		return render_template('text_noform.html', title="You're good!", message="Here is your game ID, please use this to join a new game\n"+output)
+		#{"mana_":["createGame"],"username_": "Hoa_test1","sessionID_":"TFdcXBlITLFyCAUsI7hSOxuAHZdfFYj1oVDmYvqItwY="}
+	else:
+		return render_template('text_noform.html', title="You're good!", message="Please login or create an account")
+
+
+@app.route('/win_loss', methods=['GET', 'POST'])
+def win_loss():
+	if(request.method == 'GET' and 'sessionID' in request.cookies and 'userName' in request.cookies):
+		output=sendToController('{"mana_":["viewWinLoss"],"username_": "'+request.cookies.get('userName')+'","sessionID_":"'+request.cookies.get('sessionID')+'"}')
+		#resp.set_cookie('sessionID',output)
+		if (output is None or output==""):
+			return render_template('text_noform.html', title="You're good!", message="There was some error.. ")
+		return render_template('text_noform.html', title="You're good!", message="Here is everyone's win loss\n"+output)
+		#{"mana_":["createGame"],"username_": "Hoa_test1","sessionID_":"TFdcXBlITLFyCAUsI7hSOxuAHZdfFYj1oVDmYvqItwY="}
+	else:
+		return render_template('text_noform.html', title="You're good!", message="Please login or create an account")
+
+
+@app.route('/audit', methods=['GET', 'POST'])
+def audit():
+	if(request.method == 'GET' and 'sessionID' in request.cookies and 'userName' in request.cookies):
+		output=sendToController('{"mana_":["audit"],"username_": "'+request.cookies.get('userName')+'","sessionID_":"'+request.cookies.get('sessionID')+'"}')
+		#resp.set_cookie('sessionID',output)
+		if (output is None or output==""):
+			return render_template('text_noform.html', title="You're good!", message="There was some error.. ")
+		return render_template('text_noform.html', title="You're good!", message="Here are the audit logs:\n"+output)
 		#{"mana_":["createGame"],"username_": "Hoa_test1","sessionID_":"TFdcXBlITLFyCAUsI7hSOxuAHZdfFYj1oVDmYvqItwY="}
 	else:
 		return render_template('text_noform.html', title="You're good!", message="Please login or create an account")
@@ -38,6 +69,8 @@ def new_game():
 def join_game():
 	if(request.method == 'GET' and 'sessionID' in request.cookies and 'userName' in request.cookies):
 		output=sendToController('{"game_":["listExistingGame"],"username_": "'+request.cookies.get('userName')+'","sessionID_":"'+request.cookies.get('sessionID')+'"}')
+		if (output is None or output==""):
+			return render_template('text_noform.html', title="You're good!", message="There was some error.. ")
 		return render_template('text_noform.html', title="", message=output)
 		#{"mana_":["createGame"],"username_": "Hoa_test1","sessionID_":"TFdcXBlITLFyCAUsI7hSOxuAHZdfFYj1oVDmYvqItwY="}
 	else:
@@ -46,13 +79,15 @@ def join_game():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 	if request.method == 'POST':
-		 output=sendToController('{"user_": ["Create","'+request.form['username']+'","'+request.form['password']+'"]}')
+		output=sendToController('{"user_": ["Create","'+request.form['username']+'","'+request.form['password']+'"]}')
+		if (output is None or output==""):
+			 return render_template('text_noform.html', title="You're good!", message="There was some error.. ")
 		 #print(output)
 		 #output=registration.createUser(request.form['username'],request.form['password'])
-		 resp=make_response(render_template('text_noform.html', title="You're good!", message="Registration successful, logged in with token: "+output))
-		 resp.set_cookie('sessionID',output)
-		 resp.set_cookie('userName',request.form['username'])
-		 return resp
+		resp=make_response(render_template('text_noform.html', title="You're good!", message="Registration successful, logged in with token: "+output))
+		resp.set_cookie('sessionID',output)
+		resp.set_cookie('userName',request.form['username'])
+		return resp
 	if request.method == 'GET':
 	    form = RegisterForm()
 	    if form.validate_on_submit():
@@ -65,13 +100,15 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	if request.method == 'POST':
-		 output=sendToController('{"user_": ["Authenticate","'+request.form['username']+'","'+request.form['password']+'"]}')
+		output=sendToController('{"user_": ["Authenticate","'+request.form['username']+'","'+request.form['password']+'"]}')
+		if (output is None or output==""):
+			return render_template('text_noform.html', title="You're good!", message="There was some error.. ")
 		 #print(output)
 		 #output=registration.createUser(request.form['username'],request.form['password'])
-		 resp=make_response(render_template('text_noform.html', title="You're good!", message="Authentication successful, logged in with token: "+output))
-		 resp.set_cookie('sessionID',output)
-		 resp.set_cookie('userName',request.form['username'])
-		 return resp
+		resp=make_response(render_template('text_noform.html', title="You're good!", message="Authentication successful, logged in with token: "+output))
+		resp.set_cookie('sessionID',output)
+		resp.set_cookie('userName',request.form['username'])
+		return resp
 	if request.method == 'GET':
 	    form = LoginForm()
 	    if form.validate_on_submit():
